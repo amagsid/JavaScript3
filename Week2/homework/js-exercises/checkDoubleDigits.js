fd414f0631353e266dd483527f6606fdd0977b97
Ahmad Magdy <amagsid@gmail.com>
2020-11-09 15:40:22 +0100

    fixed and tested CheckDoubleDigits.js

--- Week2/homework/js-exercises/checkDoubleDigits.js
+++ Week2/homework/js-exercises/checkDoubleDigits.js
@@ -3,12 +3,18 @@ let checkDoubleDigits = (number) => {
     
     const promise = new Promise ((resolve, reject) => {
         if (number > 10) {
-            resolve("The number is bigger than 10!");
-        } else if (num <= 10) {
-            reject("The number is smaller than 10");
-        } else if (typeof num !== number) {
-            reject(new Error,"Please enter a valid number");
+            resolve(console.log("The number is bigger than 10!") );
+        } else if (number == 10) {
+            reject(console.log("The number is 10"))
+        } else if (number < 10) {
+            reject(console.log("The number is smaller than 10"));
+        } else if (typeof number !== number) {
+            reject(new Error("Please enter a valid number"));
         }
     })
     return promise;
+    
 }
+
+//testing
+//checkDoubleDigits(11)
