50963450fdafaacb447ddbc7ca3e21e9afbe7ad9
Ahmad Magdy <amagsid@gmail.com>
2021-02-02 21:11:43 +0100

    refactored code

--- hackyourrepo-app/DomManipulation.js
+++ hackyourrepo-app/DomManipulation.js
@@ -4,23 +4,8 @@ function createElementAddClass(el, className) {
   return element;
 }
 
-function getRandomEl(arr) {
-  const randomEl = Math.floor(Math.random() * arr.length);
-  return arr[randomEl];
-}
-
-//function appendElementsToDom(houseName, lordName, houseId) {
-//  const houseList = document.getElementById(`got-house-list`);
+// function getRandomEl(arr) {
+//   const randomEl = Math.floor(Math.random() * arr.length);
+//   return arr[randomEl];
 //
-//  const gotHouse = createElementAddClass(`div`, `got-house`);
-//  const gotHouseName = createElementAddClass("h1", "got-house__title");
-//  const lordEl = createElementAddClass("span", "got-house__current-lord");
-//
-//  gotHouseName.innerText = houseName;
-//  lordEl.innerText = lordName;
-//  lordEl.id = houseId;
-//
-//  gotHouse.appendChild(gotHouseName);
-//  gotHouse.appendChild(lordEl);
-//  houseList.appendChild(gotHouse);
-//}
+function appendElementsToDom(houseName, lordName, houseId) {}
--- hackyourrepo-app/app.js
+++ hackyourrepo-app/app.js
@@ -1,1 +1,26 @@
-console.log('hey');
+//'use strict';
+//console.log(placeholderRepos);
+const optionEl = document.getElementById('repo-select');
+const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+
+window.onload = async function main() {
+  const reposInfo = await fetchData(url);
+  optionEl.innerHTML = reposInfo
+    .map((repo) => {
+      return `<option data-id="${repo.id}"> ${repo.name} </option>`;
+    })
+    .join('');
+};
+
+optionEl.addEventListener('change', async () => {
+  console.log(optionEl.value);
+  const repoData = await fetchData(
+    `https://api.github.com/repos/HackYourFuture/${optionEl.value}`,
+  );
+
+  const { name, description, updated_at, forks, contributors_url } = repoData;
+
+  console.log(name, description, updated_at, forks, contributors_url);
+
+  appendElementsToDom(repoData);
+});
